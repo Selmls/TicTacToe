237607757fc49a2df6d3e409a2a485e136f262c9
Eimis <Eimis4k@gmail.com>
2023-12-12 05:15:26 -0800

    added check_win

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-from models.pirmas import pirmas, lista, antras, trecias
+from models.pirmas import pirmas, lista, antras, trecias, check_win
 while True:
     try:
         a = int(input("1 - Žaisti dviems\n2 - Pakaroti žaidimą\n3 - Žaisti prieš kompiuterį\n4 - Baigti\n"))
-        break
     except ValueError:
         print("Nėra tokio pasirinkimo")
-match a:
-    case 1:
-        print(lista)
-        b = pirmas()
-        while True:
-            c = antras(b)
-            b = trecias(c)
-            if c == or b ==
-    case 2:
-        ...
-    case 3:
-        ...
-    case 4:
-        ...
+    match a:
+        case 1:
+            print(lista)
+            b = pirmas()
+            while True:
+                c = antras(b)
+                b = trecias(c)
+                if check_win(b) or check_win(c):
+                    break
+        case 2:
+            ...
+        case 3:
+            ...
+        case 4:
+            ...
 
 
--- models/pirmas.py
+++ models/pirmas.py
@@ -39,27 +39,33 @@ def trecias(listas):
         print("Wrong value")
 
 
-def ketvirtas(listas):
-    o = input("enter number")
-    if o in listas:
-        listas = listas.replace(o, "0")
-        print(listas)
-        return listas
-    elif o not in listas:
-        print(listas)
-        print("Wrong value")
+def check_win(listas):
+    lines = listas.split('\n')
 
+    # Check rows
+    for line in lines:
+        if 'XXX' in line:
+            return print('X won')
+        elif '000' in line:
+            return print('0 won')
 
-def penktas(listas):
-    x = input("enter number")
-    if x in listas:
-        xreiksmes.append(x)
-        listas = listas.replace(x, "X")
-        print(listas)
-        return listas
-    elif x not in listas:
-        print(listas)
-        print("Wrong value")
+    # Check columns
+    for col in range(len(lines[0])):
+        column = ''.join(row[col] for row in lines if col < len(row))
+        if 'XXX' in column:
+            return print('X won')
+        elif '000' in column:
+            return print('0 won')
+
+    # Check diagonals
+    diagonal1 = ''.join(lines[i][i] for i in range(len(lines)) if i < len(lines[i]))
+    diagonal2 = ''.join(lines[i][len(lines) - 1 - i] for i in range(len(lines)) if len(lines[i]) > len(lines) - 1 - i)
+
+    if 'XXX' in diagonal1 or 'XXX' in diagonal2:
+        return print('X won')
+    elif '000' in diagonal1 or '000' in diagonal2:
+        return print('0 won')
+
 
 
 
